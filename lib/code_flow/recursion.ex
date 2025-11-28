fbabbd182a90228a8c99f1e697a72385ba50cf53
Daigo Shitara <stain.witness+github@gmail.com>
2025-11-28 11:03:07 +0900

    recursion

--- lib/code_flow/recursion.ex
+++ lib/code_flow/recursion.ex
@@ -10,31 +10,58 @@ defmodule CodeFlow.Recursion do
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
-  # alias CodeFlow.Fake.Customers
-  # alias CodeFlow.Schemas.OrderItem
+
+  alias CodeFlow.Fake.Customers
+  alias CodeFlow.Schemas.Customer
+  alias CodeFlow.Schemas.OrderItem
 
   @doc """
   Sum a list of OrderItems to compute the order total.
   """
-  def order_total(_order_items) do
-
+  def order_total(order_items) do
+    aux_order_total(order_items, 0)
   end
+
+  defp aux_order_total([%OrderItem{} = order_item | tail], acc) do
+    aux_order_total(tail, acc + order_item.item.price * order_item.quantity)
+  end
+
+  defp aux_order_total([], acc), do: acc
 
   @doc """
   Count the number of active customers. Note: Normally this would be done with a
   query to an SQL database. This is just to practice conditionally incrementing
   a counter and looping using recursion.
   """
-  def count_active(_customers) do
-
+  def count_active(customers) do
+    aux_count_active(customers, 0)
   end
+
+  defp aux_count_active([%Customer{active: true} | rest], acc) do
+    aux_count_active(rest, acc + 1)
+  end
+
+  defp aux_count_active([_ | rest], acc) do
+    aux_count_active(rest, acc)
+  end
+
+  defp aux_count_active([], acc), do: acc
 
   @doc """
   Create the desired number of customers. Provide the number of customers to
   create. Something like this could be used in a testing setup.
   """
-  def create_customers(_number) do
+  def create_customers(total) do
+    aux_create_customers(total, 0)
+  end
 
+  defp aux_create_customers(total, num) when num < total do
+    {:ok, _new_customer} = Customers.create(%{name: "Customer #{num}"})
+    aux_create_customers(total, num + 1)
+  end
+
+  defp aux_create_customers(total, _num) do
+    "Created #{total} customers!"
   end
 
   @doc """
@@ -43,7 +70,10 @@ defmodule CodeFlow.Recursion do
   sequence.
   https://en.wikipedia.org/wiki/Fibonacci_number
   """
-  def fibonacci(_num) do
+  def fibonacci(0), do: 0
+  def fibonacci(1), do: 1
 
+  def fibonacci(num) do
+    fibonacci(num - 2) + fibonacci(num - 1)
   end
 end
---
+++ play/recursion.exs
@@ -0,0 +1,37 @@
+defmodule Play do
+  def process([head | rest]) do
+    IO.puts("Value of num  = #{head}")
+    process(rest)
+  end
+
+  def process([]), do: IO.puts("Done!")
+
+  def sum([head | tail], acc) do
+    sum(tail, acc + head)
+  end
+
+  def sum([], acc), do: acc
+
+  def build_text(number) do
+    aux_build_text(number, 0, "")
+  end
+
+  defp aux_build_text(total, num, text) when num < total do
+    aux_build_text(
+      total,
+      num + 1,
+      text <> "Processed number #{num}\n"
+    )
+  end
+
+  defp aux_build_text(_total, _num, text), do: text
+end
+
+data = [1, 2, 3, 4, 5]
+data |> Play.process()
+
+sum_result = data |> Play.sum(0)
+"Sum of numbers = #{sum_result}" |> IO.puts()
+
+text = Play.build_text(5)
+text |> IO.puts()
